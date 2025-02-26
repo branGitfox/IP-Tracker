e182bf712ae2cf71639a691156c0f59a20268eb7
BranGitfox <vixfgit@gmail.com>
2025-02-26 23:41:40 +0300

    built: add react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,14 @@ function App() {
 
   return (
     <>
-     
+     <div className="navbar bg-blue-900">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+      
+  </div>
+</div>
     </>
   )
 }
